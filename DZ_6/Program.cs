2ee6e28c7c5abb89c89ab09bf2c9343e3a06f31f
Grisha_Pinigin <pin.pinigin@mail.ru>
2022-08-23 22:11:15 +0500

    Задача 6

---
+++ DZ_6/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).

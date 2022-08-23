07b5326c1763d3aec9d2b6b001ca591e5adfbdda
Grisha_Pinigin <pin.pinigin@mail.ru>
2022-08-23 23:19:45 +0500

    Задача 6

--- DZ_8/Program.cs
+++ DZ_8/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while(i <= number)
+{
+   if(i % 2 == 0)
+   {
+        Console.WriteLine(i);
+   }
+    i++;
+}
